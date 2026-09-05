72b59431f0603daf3cc1f69ec1fc713913794d53
hpot10008-art <hpot10008@gmail.com>
2026-09-06 02:02:58 +0300

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.sansu.aiforexbot"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.sansu.aiforexbot"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
