0e7aaf602818b098a2914f7dc46df2f3bedcbd6d
hpot10008-art <hpot10008@gmail.com>
2026-09-06 02:43:41 +0300

    Create settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Sansu-AI-Forex-Bot"
+include(":app")
