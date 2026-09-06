9dba7a5568aeb830b1026b07d1555f448c59a80b
hpot10008-art <hpot10008@gmail.com>
2026-09-06 05:10:55 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,18 @@
+
 plugins {
-    id("com.android.application") version "8.7.3" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.sansu.aiforexbot"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.sansu.aiforexbot"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
 }
