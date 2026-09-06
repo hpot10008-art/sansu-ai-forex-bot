22d58116ea87c235ed6bd4fc4ff355ca9ac58288
hpot10008-art <hpot10008@gmail.com>
2026-09-06 05:20:30 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     id("com.android.application") version "8.7.3"
     id("org.jetbrains.kotlin.android") version "2.0.21"
 }
-
 android {
     namespace = "com.sansu.aiforexbot"
     compileSdk = 35
